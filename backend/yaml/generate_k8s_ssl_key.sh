4bce95aae1fead29a568d38b42c1c2b275a5a04d
duhyun <duhyun@localhost.localdomain>
2023-06-21 09:02:39 +0900

    change ingress sh, yaml file

--- backend/yaml/generate_k8s_ssl_key.sh
+++ backend/yaml/generate_k8s_ssl_key.sh
@@ -1,5 +1,11 @@
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=10.10.27.17.nip.io"
+#인증서 생성
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=10.10.27.17.nip.io/O=10.10.27.17.nip.io"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout key.pem -out cert.pem -subj "/C=US/ST=State/L=City/O=Organization/CN=10.10.27.17.nip.io" -addext "subjectAltName = DNS:10.10.27.17.nip.io, IP:10.10.27.17"
+
+
 
 sleep 1
 
-kubectl create secret tls sping.secret.key --key tls.key --cert tls.crt
+#Secret 생성
+#kubectl create secret tls spring.secret.key -n covi --key tls.key --cert tls.crt
+kubectl create secret tls spring.secret.key -n covi --key key.pem --cert cert.pem
--- backend/yaml/ingress.sh
+++ backend/yaml/ingress.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # http
-#/usr/local/bin/kubectl port-forward --address 0.0.0.0 deployment/ingress-nginx-controller 80:80 -n ingress-nginx
-/usr/local/bin/kubectl port-forward --address 0.0.0.0 deployment/springmvc 8081:8081 -n duhyun
+/usr/local/bin/kubectl port-forward --address 0.0.0.0 deployment/ingress-nginx-controller 443:443 -n ingress-nginx
+#/usr/local/bin/kubectl port-forward --address 0.0.0.0 service/springmvc 443:443 -n covi
+
